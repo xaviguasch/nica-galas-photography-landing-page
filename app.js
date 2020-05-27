3833683eb9048ea8df1dba54c8c659b76c1e1cf8
xaviguasch <xaviguasch@gmail.com>
2020-05-27 17:47:48 +0200

    added smooth scroll

--- app.js
+++ app.js
@@ -4,4 +4,12 @@ const links = nav.querySelectorAll('a')
 
 burger.addEventListener('click', () => {
   nav.classList.toggle('nav-open')
+  burger.classList.toggle('toggle')
+})
+
+links.forEach((link) => {
+  link.addEventListener('click', () => {
+    nav.classList.toggle('nav-open')
+    burger.classList.toggle('toggle')
+  })
 })
